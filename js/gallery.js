27a1e60da03313f2982cd24c80f196833a1444a3
mathias <mathiastortomano.dev@gmail.com>
2023-07-20 03:38:56 -0300

    agregando funcionalidad de siguiente y anterior imagen al modal de la galeria, tambien se renombraron todas las imagenes de la misma

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,8 @@
+
+// open gallery image with modal 
 let imgs = document.querySelectorAll(".gallery__img");
 let modal__container = document.querySelector(".modal__container");
 let modal__img = document.querySelector(".modal__img");
-let modal__close = document.querySelector(".modal__close__icon__container");
-
-let load__more__buttons = document.querySelectorAll(".load__more");
-let gallery__containers = document.querySelectorAll(".gallery__container__hidden");
 
 imgs.forEach((img) => {
   img.addEventListener("click", () => {
@@ -16,10 +14,6 @@ imgs.forEach((img) => {
   });
 });
 
-modal__close.addEventListener("click", () => {
-  modal__container.style.display = "none";
-});
-
 modal__container.addEventListener("click", () => {
   modal__container.style.display = "none";
 })
@@ -27,6 +21,43 @@ modal__container.addEventListener("click", () => {
 modal__img.addEventListener('click', (e) => {
   e.stopPropagation();
 })
+
+
+// modal icons events
+let modal__close = document.querySelector(".modal__close__icon__container");
+let modal__arrow__right = document.querySelector(".modal__arrow__icon-right");
+let modal__arrow__left = document.querySelector(".modal__arrow__icon-left");
+
+modal__close.addEventListener("click", () => {
+  modal__container.style.display = "none";
+});
+
+
+function ModalArrowChange(e, change) {
+  e.stopPropagation();
+    let imgRoute = modal__img.getAttribute("src");
+
+    // la expresion regular /\d+/ busca el número en la ruta de la imagen, y [0] accede al primer elemento del array devuelto por match()
+    const currentNumberRouteImg = parseInt(imgRoute.match(/\d+/)[0]);
+    const nextNumberRouteImg = currentNumberRouteImg + change;
+    let nextImgRoute = imgRoute.replace(currentNumberRouteImg, nextNumberRouteImg);
+
+    modal__img.setAttribute("src", nextImgRoute);
+}
+
+modal__arrow__right.addEventListener("click", (e) => {
+  ModalArrowChange(e, 1)
+})
+
+modal__arrow__left.addEventListener("click", (e) => {
+  ModalArrowChange(e, -1)
+})
+
+
+
+// load more images
+let load__more__buttons = document.querySelectorAll(".load__more");
+let gallery__containers = document.querySelectorAll(".gallery__container__hidden");
 
 for (let i = 0; i < load__more__buttons.length; i++) {
   load__more__buttons[i].addEventListener("click", () => {
--- php/gallery.php
+++ php/gallery.php
@@ -21,7 +21,17 @@
                 <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
             </svg>
         </div>
+        <div class="modal__arrow__icon__container modal__arrow__icon-left__container">
+            <svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 0 320 512" class="modal__arrow__icon modal__arrow__icon-left">
+                <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+            </svg>
+        </div>
         <img class="modal__img">
+        <div class="modal__arros__icon__container modal__arros__icon-right__container">
+            <svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 0 320 512" class="modal__arrow__icon modal__arrow__icon-right">
+                <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
+            </svg>
+        </div>
     </div>
 
     <header>
@@ -87,7 +97,7 @@
             echo "<section class='gallery__container'>";
             echo "<div class='imgs__container'>";
             foreach ($imgs as $key => $img) {
-                echo "<style> #img" . $key . "{background-image:url('" . $img . "');} </style>";
+                echo "<style> #img" . $key . "{background-image:url('../imgs/gallery/img" . $key + 1 . ".webp');} </style>";
                 echo "<div class='gallery__img' id='img" . $key . "'> </div>";
                 if (($key + 1) % 48 == 0) {
                     echo "</div>";
