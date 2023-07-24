c41f63817fc36fbca546791b43f441302ca3f879
Brandon Calimares <brandoncalimares@gmail.com>
2023-07-23 21:10:11 -0300

    Correccion ortografica galeria

--- php/gallery.php
+++ php/gallery.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Galeria</title>
+    <title>Galería</title>
     <link rel="shortcut icon" href="../imgs/icons/logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/header.css">
