3141877fe693b2bbac1b3ba5fd9353f8f35006bf
mathias <mathiastortomano.dev@gmail.com>
2023-09-21 18:36:17 -0300

    comienzo de rediseño mas intuitivo y moderno: seccion de inicio completada, se vinculo la seccion de contacto con una api de google maps para visualizar el mapa de donde se encuentra la CEDEC, . ademas de todo esto se modularizaron las imagenes en subcarpetas.

--- js/header.js
+++ js/header.js
@@ -1,3 +1,22 @@
+
+let headerContainer = document.querySelector(".header");
+let headerLogo = document.querySelector(".nav__logo__container");
+
+const checkScrolled = () => {
+    if (window.scrollY > 0) {
+        headerContainer.classList.add("header-active");
+        headerLogo.classList.add("nav__logo-active")
+    } else {
+        headerContainer.classList.remove("header-active");
+        headerLogo.classList.remove("nav__logo-active")
+    }
+}
+
+window.addEventListener("scroll", checkScrolled);
+window.addEventListener("load", checkScrolled);
+
+
+
 const headerOpen = document.querySelector(".header__nav__open__icon");
 const headerClose = document.querySelector(".header__nav__close__icon");
 const headerLinksContainer = document.querySelector(".header__links__container");
