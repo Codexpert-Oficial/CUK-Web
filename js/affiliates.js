887ae8c0f1a6344e32128418b9fb633e21b5e4db
mathias <mathiastortomano.dev@gmail.com>
2023-10-11 19:23:41 -0300

    agregando seccion de afiliadas e implementando funcionalidad al modal que despliega la informacion de cada institucion, tanto en la seccion de afiliadas como en la seccion de inicio

---
+++ js/affiliates.js
@@ -0,0 +1,201 @@
+const data = {
+    "akkhu": {
+        "Referente": "Sensei Roque Pereyra (5º DAN)",
+        "Email": "roquepereyra@hotmail.com",
+        "Dirección": "Hombu Dojo Guayabos 1606 Esq. Carlos Roxlo",
+        "Teléfonos": ["23111972", "095 331573"],
+        "Redes Sociales": ["kyudokanhigateuruguay", " kyudokanhigatedeluruguay"]
+    },
+    "akku": {
+        "Referente": "Radko Balcar",
+        "Email": "rbalcar@gmail.com",
+        "Dirección": "Canelones 777, Montevideo",
+        "Teléfono": "099 375 908",
+        "Sitio web": "www.uruguaygojuryu.com"
+    },
+    "aklm": {
+        "Referente": "José Eduardo García",
+        "Email": "garciainchorbe@gmail.com",
+        "Dirección": "Miguelete 1567",
+        "Teléfonos": ["29254583", "096 852 075"],
+        "Sitio web": "www.kyokushinuruguay"
+    },
+    "aku": {
+        "Referente": "Luis Costa",
+        "Email": "asociacionkyudokanuruguay@gmail.com",
+        "Dirección": "Prusia 1617 - Club Verdirrojo.",
+        "Teléfonos": ["098 708 809", "091 52 40 52"],
+        "Redes Sociales": "shorinryukyudokanuruguay"
+    },
+    "alpha": {
+        "Referente": "Rodrigo Maneiro",
+        "Email": "gerenciaalphagym@gmail.com",
+        "Dirección": "Avda Roosevelt esquina Miguel Ángel Moscoso. Maldonado",
+        "Teléfono": "4224 3991",
+        "Redes Sociales": ["AlphaClubMaldonado", "alphaclubuy"],
+        "Sitio web": "www.alphaclub.uy"
+    },
+    "ashram": {
+        "Referente": "Miguel Correa - 5º Dan",
+        "Sitio web": "http://www.shoburyu.com.uy",
+        "Email": "info@shoburyu.com.uy",
+        "Dirección": "Marrubio 3982",
+        "Teléfonos": ["2220 0620", "099 913 399"]
+    },
+    "auk": {
+        "Referente": "Juan Manuel Alonso - 7° Dan",
+        "Facebook": "AsociacionUruguayaDeKarate",
+        "Email": "info@auk.com.uy",
+        "Dirección": "José Luis Bado 3186 (Club 25 de Agosto)",
+        "Teléfono": "099 631 512"
+    },
+    "bk": {
+        "Referente": "Marcelo Carambula"
+    },
+    "ifk": {
+        "Referente": "Shihan Germán Carballo 5to Dan",
+        "Email": "gcarballo.ifkuruguay@gmail.com",
+        "Dirección": "Pernas 2360",
+        "Teléfono": "097 308 442",
+        "Redes Sociales": ["ifk.uruguay", "Ifkuruguay"],
+        "Sitio web": "www.ifk-uruguay.com"
+    },
+    "iktu": {
+        "Presidente": "Jorge A. Crosa - 4º Dan ITKF",
+        "Email": "secretariado@iktu.org.uy",
+        "Dirección": "Guttemberg 6133",
+        "Teléfono": "099 033 008",
+        "Redes Sociales": "iktukarate",
+        "Sitio web": "www.iktu.org.uy"
+    },
+    "intendenciamaldonado": {
+        "Referente": "Director General de Deportes - Sr. Martín Hualde",
+        "Email": "dirgraldeportes@maldonado.gub.uy"
+    },
+    "iskf": {
+        "Referente": "Ruben Edgardo Auza",
+        "Email": "iskfuruguayuuk@gmail.com",
+        "Dirección": "Ecuador 1680",
+        "Teléfono": "094 532667",
+        "Sitio web": "ISKF Uruguay",
+        "Redes Sociales": "iskf_uruguay"
+    },
+    "jitsu": {
+        "Referente": "Omar Díaz",
+        "Email": "eukj.uy@gmail.com",
+        "Dirección": "San Jose de Carrasco - Canelones"
+    },
+    "ketsugo": {
+        "Referentes": ["Jorge Mercado 6º DAN", "Pablo Layerla 4ºDAN"],
+        "Emails": ["karateketsugouy@gmail.com", " pablolayerla@hotmail.com"],
+        "Dirección": "Lavalleja Km.23,400 - Gimnasio Alquimia / Solymar - Canelones - Uruguay",
+        "Teléfonos": ["095 431 056", "095 342 373", "094 776577"],
+        "Redes Sociales": "Karate-Ketsugo-Ciudad-de-la-Costa",
+        "Sitio web": "karateketsugouruguay"
+    },
+    "ksk": {
+        "Sensei": "Roberto Chocho Maglio",
+        "Email": "karateksk@gmail.com",
+        "Dirección": "Ruta Interbalneraria, Km. 46.800",
+        "Teléfonos": ["43726604", "091 060802"],
+        "Sitio web": "www.kenshinkanuruguay.com.uy"
+    },
+    "kkk": {
+        "Referente": "Walter Urruzola",
+        "Email": "karatekyokukaikankyokushin@gmail.com"
+    },
+    "kyokushin": {
+        "Referente": "Carlos Díaz",
+        "Dirección": "Av. Uruguay 1719, Montevideo",
+        "Teléfono": "095 929 061",
+        "Redes Sociales": ["kyokushinunion.uruguay", "kyokushinunionuruguay"],
+        "Sitio web": "www.karate.uy"
+    },
+    "ofku": {
+        "Referente": "Norberto Sena - 4to Dan",
+        "Email": "info@ofku.org",
+        "Dirección": "Av. Luis Batlle Berres 6055",
+        "Teléfono": "098279842",
+        "Redes Sociales": "ofkuruguay"
+    },
+    "shingitai": {
+        "Referente": "Shihan CARLOS PRELIASCO",
+        "Email": "shingitaiuruguay@gmail.com",
+        "Dirección": "Carlos María Ramírez 2162 1er. Piso",
+        "Teléfonos": ["2311 7571", "099 667 637"],
+        "Redes Sociales": "adriana.carlos.sgt"
+    },
+    "shinshukan": {
+        "Representante": "Juan Carlos Borbas - 6º Dan Karate - 3º Dan Kobudo. Representante para Uruguay IUSKF",
+        "Email": "shinshukanuruguay@hotmail.com",
+        "Dirección": "Calle 15/3682 Casabó-Cerro",
+        "Teléfonos": ["2314 6668", "095 318 062"]
+    },
+    "skif": {
+        "Sensei": "Carlos Pazos",
+        "Email": "skifuruguay@gmail.com",
+        "Dirección": "Canelones 1583/903",
+        "Teléfono": "097 969860",
+        "Redes Sociales": "SKIFUruguay",
+        "Sitio web": "skifuruguay"
+    },
+    "sok": {
+        "Representante": "Prof, Erasmo Petingi Labastie - 6º Dan. Shotokan. Miembro Activo de IOSTK",
+        "Email": "sok@adinet.com.uy"
+    },
+    "unk": {
+        "Sensei": "Oscar Villarreal - Director Nacional",
+        "Dirección": "Jose Rotondaro 1609 - Toledo - Canelones",
+        "Teléfono": "094 269 122"
+    },
+    "ufko": {
+        "Referente": "José Eduardo García",
+        "Email": "garciainchorbe@gmail.com",
+        "Dirección": "Fernandez Crespo 1716",
+        "Teléfonos": ["24094489", "096 852 075"]
+    }
+
+}
+
+const modalContainer = document.querySelector(".modal__container");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modal__img");
+const modalDescription = document.querySelector(".modal__description");
+const modalIconClose = document.querySelector(".modal__close__icon");
+
+modalContainer.addEventListener("click", () => {
+    modalContainer.classList.remove("modal__container-active")
+});
+
+modalIconClose.addEventListener("click", () => {
+    modalContainer.classList.remove("modal__container-active")
+});
+
+modal.addEventListener("click", (e) => e.stopPropagation())
+
+const changeModalData = (organizationName) => {
+    const organization = data[organizationName];
+
+    if (organization) {
+        modalDescription.innerHTML = "";
+        for (const property in organization) {
+            if (organization.hasOwnProperty(property)) {
+                modalDescription.innerHTML += `<p> ${property}: ${organization[property]} </p>`;
+                modalImg.src = `../imgs/logos/${organizationName}.png`;
+            }
+        }
+    }
+}
+
+const affiliatesElements = document.querySelectorAll(".affiliates__element");
+const affiliatesElementsTitles = document.querySelectorAll(".affiliates__element__title");
+
+affiliatesElements.forEach((element, index) => {
+    element.addEventListener("click", () => {
+        modalContainer.classList.add("modal__container-active")
+        changeModalData(affiliatesElementsTitles[index].textContent)
+    })
+});
+
+
+
