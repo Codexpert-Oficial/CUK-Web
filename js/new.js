42e2500bc5417844f4fb250f5549d273302c9077
mathias <mathiastortomano.dev@gmail.com>
2023-11-05 16:14:58 -0300

    se implemento una mejora en el metodo de mostrar las noticias individualmente, ahora solo se necesita un solo archivo html para visualizar cada noticia. tambien se soluciono un problema visual en la seccion de inicio

---
+++ js/new.js
@@ -0,0 +1,208 @@
+const news = [
+    {
+        "title": "La Confederación Uruguaya de Karate felicita a Joel Mókosce",
+        "texts": [
+            "La Confederación Uruguaya de Karate felicita a Joel Mókosce (Presidente del Consejo Arbitral), quien fuera designado cómo árbitro para representar a nuestro país en los Juegos Panamericanos de Santiago 2023."
+        ],
+        "images": [
+            "../imgs/news/new1.jpg"
+        ]
+    },
+    {
+        "title": "TAKUYA MAKITA, Sensei en Uruguay",
+        "texts": [
+            "¡Únete al Seminario Internacional TAKUYA MAKITA SENSEI! Del 4 al 6 de julio en varios departamentos. Curso abierto a todos."
+        ],
+        "images": [
+            "../imgs/news/new2-img1.jpg",
+            "../imgs/news/new2.png",
+        ]
+    },
+    {
+        "title": "Seminario IOSTK Internacional",
+        "texts": [
+            "Participa en el Seminario Internacional IOSTK con SHIHAN ANGEL ARENAS. Del 21 al 28 de agosto de 2023. Informes: 098 531 172."
+        ],
+        "images": [
+            "../imgs/news/new3.jpeg",
+            "../imgs/news/new3-img1.jpeg"
+        ]
+    },
+    {
+        "title": "Lanzamiento del Libro: MANUAL DE KARATE-DO “El Arte de la No Violencia”.",
+        "texts": [
+            "Únete al lanzamiento del libro “MANUAL DE KARATE-DO: El Arte de la No Violencia”. Sábado 18 de marzo en CEDEC. ¡No faltes!"
+        ],
+        "images": [
+            "../imgs/news/new4-img1.jpg",
+            "../imgs/news/new4.jpg",
+            "../imgs/news/new4-img3.jpg",
+            "../imgs/news/new4-img4.jpg",
+            "../imgs/news/new4-img5.jpg",
+            "../imgs/news/new4-img6.jpg",
+        ]
+    },
+    {
+        "title": "Jornada de Actividades e Intercambios a cara a un nuevo 8 de Marzo",
+        "texts": [
+            "¡Únete al Seminario Internacional TAKUYA MAKITA SENSEI! Del 4 al 6 de julio en varios departamentos. Curso abierto a todos."
+        ],
+        "images": [
+            "../imgs/news/new5.jpg",
+        ]
+    },
+    {
+        "title": "2do Torneo Nacional de Fullcontact Karate",
+        "texts": [
+            "OFKU te invita al 2° Torneo Nacional de Fullcontact Karate, el 21 de mayo en Montevideo. ¡Únete y vive este emocionante evento!"
+        ],
+        "images": [
+            "../imgs/news/new6-img1.jpg",
+            "../imgs/news/new6.jpeg",
+            "../imgs/news/new6-img2.jpeg",
+            "../imgs/news/new6-img3.jpeg",
+            "../imgs/news/new6-img4.jpeg",
+            "../imgs/news/new6-img5.jpeg",
+            "../imgs/news/new6-img6.jpeg",
+            "../imgs/news/new6-img7.jpeg"
+        ]
+    },
+    {
+        "title": "Karate Tradicional Campeonato Apertura 2023",
+        "texts": [
+            "Exhibiciones de artes marciales, Campeonato Apertura 2023 y Festival Infantil Dangai el 16 de abril en La Par. Entrada libre."
+        ],
+        "images": [
+            "../imgs/news/new7.jpeg",
+            "../imgs/news/new7-img1.jpeg"
+        ]
+    },
+    {
+        "title": "Panamericano 2024 en Punta del Este",
+        "texts": [
+            "¡Prepárate para el emocionante Panamericano 2024 en Punta del Este! Más detalles próximamente."
+        ],
+        "images": [
+            "../imgs/news/new8.jpg"
+        ]
+    },
+    {
+        "title": "Curso Avanzado de Gestión Deportiva",
+        "texts": [
+            "Comenzó el día 27 de Abril, la 2da Edición del Curso Avanzado de Gestión Deportiva organizado por el Comité Olímpico Uruguayo, auspiciado por el Departamento de Solidaridad Olímpica del Comité Olímpico Internacional.",
+            "Dicha actividad, dirigida a representantes de las federaciones afiliadas al COU, aborda en profundidad seis ámbitos de la gestión deportiva y tiene como principal objetivo proporcionar habilidades y herramientas necesarias que permitan un mejor desarrollo de gestión en los sistemas organizativos."
+        ],
+        "images": [
+            "../imgs/news/new9.jpeg",
+            "../imgs/news/new9-img1.jpeg",
+            "../imgs/news/new9-img2.jpeg"
+        ]
+    },
+    {
+        "title": "JUEGOS PARAPANAM - SANTIAGO 2023",
+        "texts": [
+            "Santiago será el escenario de los Juegos Parapanam, un evento deportivo con un enfoque especial en la promoción de la paz y la seguridad."
+        ],
+        "images": [
+            "../imgs/news/new10.jpg",
+            "../imgs/news/new10-img2.jpeg",
+            "../imgs/news/new10-img3.jpeg",
+            "../imgs/news/new10-img4.jpeg",
+            "../imgs/news/new10-img5.jpeg",
+            "../imgs/news/new10-img6.jpeg",
+            "../imgs/news/new10-img7.jpeg",
+            "../imgs/news/new10-img8.jpeg",
+            "../imgs/news/new10-img9.jpeg",
+            "../imgs/news/new10-img10.jpeg",
+            "../imgs/news/new10-img11.jpeg",
+            "../imgs/news/new10-img12.jpeg",
+            "../imgs/news/new10-img13.jpeg",
+            "../imgs/news/new10-img14.jpeg",
+            "../imgs/news/new10-img15.jpeg",
+            "../imgs/news/new10-img16.jpeg",
+            "../imgs/news/new10-img17.jpeg",
+            "../imgs/news/new10-img18.jpeg",
+        ]
+    },
+    {
+        "title": "PIN DE ORO - SENADOR DE LA REPÚBLICA EC. SERGIO BOTANA",
+        "texts": [
+            "El Comité Olímpico Uruguayo distinguió el día 26 de Abril al Senador de la República, EC. Sergio Botana con el otorgamiento del Pin de Oro por su contribución al desarrollo del deporte olímpico.",
+            "Cabe recordar que el Senador Botana impulsó la ley por la cual se le asignan recursos al Comité Olímpico Uruguayo para la preparación de atletas para el Ciclo Olímpico, Juegos Sudamericanos, Juegos Panamericanos y Juegos Olímpicos."
+        ],
+        "images": [
+            "../imgs/news/new11.jpeg",
+            "../imgs/news/new11-img1.jpeg",
+            "../imgs/news/new11-img2.jpeg"
+        ]
+    },
+    {
+        "title": "Campeonato Panamericano de Karate en Uruguay",
+        "texts": [
+            "Uruguay será anfitrión del emocionante Campeonato Panamericano de Karate. ¡Prepárate para el gran evento deportivo!"
+        ],
+        "images": [
+            "../imgs/news/new12.jpg"
+        ]
+    },
+    {
+        "title": "Escuela de Karate",
+        "texts": [
+            "Si siempre has querido aprender karate, ahora es tu momento. Descubre nuestros cursos de iniciación gratuitos, bajo la dirección del maestro Maximiliano Larrosa.",
+            "Escuela de Karate dictada en el Centro de deportes de combate."
+        ],
+        "images": [
+            "../imgs/news/new13.jpg"
+        ]
+    },
+    {
+        "title": "Workshops de Karate Tradicional",
+        "texts": [
+            "Te invitamos al evento académico WORKSHOPS DE KARATE TRADICIONAL en Maldonado, 28-29 de julio. ¡Participa en esta experiencia única!"
+        ],
+        "images": [
+            "../imgs/news/new14.jpg",
+            "../imgs/news/new14-img1.jpg"
+        ]
+    },
+    {
+        "title": "Seminario Técnico",
+        "texts": [
+            "¡Asegura tu lugar en el Seminario Técnico con SENSEI DANIEL SOTO GONZÁLEZ! BUNKAI de técnicas tradicionales de Karate el 1 de septiembre en CEDEC. Inscripciones abiertas."
+        ],
+        "images": [
+            "../imgs/news/new15.jpg",
+        ]
+    },
+];
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const newIndex = urlParams.get('id');
+
+const seeNew = (index) => {
+    const newTitle = document.querySelector(".new__title");
+    const newDescription = document.querySelector(".new__description");
+    const newImagesContainer = document.querySelector(".new__imgs__container");
+
+    const newData = news[index];
+
+    newTitle.textContent = newData.title;
+
+    newData.texts.forEach(text => {
+        const p = document.createElement("p");
+        p.textContent = text;
+        newDescription.appendChild(p);
+    });
+
+    newData.images.forEach(imageURL => {
+        const img = document.createElement("img");
+        img.classList.add("new__img")
+        img.src = imageURL;
+        newImagesContainer.appendChild(img);
+    });
+
+}
+
+seeNew(newIndex);
+
